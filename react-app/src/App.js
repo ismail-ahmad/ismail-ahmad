6e5f67b305eff4bcda05fa71c42373e9d70c3076
ismail-ahmad <ismailahmad0505@gmail.com>
2023-04-19 16:24:15 +0500

    commit

---
+++ react-app/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './layout.js';
+import About from './about.js';
+import Contact from './contact.js';
+import Home from './home.js';
+import Blogroutes from './blogroutes.js';
+import Notfound from './notfound';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/blog/*" element={<Blogroutes />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+        <Route path="*" element={<Notfound />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
